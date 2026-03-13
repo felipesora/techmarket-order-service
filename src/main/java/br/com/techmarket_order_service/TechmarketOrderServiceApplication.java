e9bf3e4c1a783a9cc22bd2b52c5c1d56574b3886
felipesora <felipeusora@gmail.com>
2026-03-12 23:35:34 -0300

    criando microsservico de pedidos

---
+++ src/main/java/br/com/techmarket_order_service/TechmarketOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.techmarket_order_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechmarketOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechmarketOrderServiceApplication.class, args);
+	}
+
+}
