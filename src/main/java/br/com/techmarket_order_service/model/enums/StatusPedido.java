6b6143aaab22f53e9e9bc5dccfe26c36b390b258
felipesora <felipeusora@gmail.com>
2026-03-18 14:42:50 -0300

    feat: criando pedido com status AGUARDANDO_PAGAMENTO

--- src/main/java/br/com/techmarket_order_service/model/enums/StatusPedido.java
+++ src/main/java/br/com/techmarket_order_service/model/enums/StatusPedido.java
@@ -1,11 +1,9 @@
 package br.com.techmarket_order_service.model.enums;
 
 public enum StatusPedido {
-    CRIADO,
     AGUARDANDO_PAGAMENTO,
     PAGAMENTO_APROVADO,
     PAGAMENTO_RECUSADO,
-    PROCESSANDO,
     ENVIADO,
     ENTREGUE,
     CANCELADO
